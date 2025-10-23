98199ae3cc1d485d44718f4bd021aacf688d8e1d
Drago00132 <jeusariasortiz91@gmail.com>
2025-10-23 13:54:13 -0500

    1:54pm

--- innerJoin.sql
+++ innerJoin.sql
@@ -23,8 +23,44 @@ FROM orden_servicio o
 INNER JOIN administradores a ON o.ID_ADMINISTRADOR = a.ID_ADMINISTRADOR
 INNER JOIN clientes c ON o.ID_CLIENTES = c.ID_CLIENTES
 INNER JOIN tecnicos t ON o.ID_TECNICOS = t.ID_TECNICOS
-INNER JOIN motos m    ON o.ID_MOTOS =  m.ID_MOTOS
-
-
-
-
+INNER JOIN motos m    ON o.ID_MOTOS =  m.ID_MOTOS;
+/* Todos los clientes con su ubicacion */
+SELECT c.Nombre, u.Ciudad, u.Direccion
+FROM clientes c 
+JOIN ubicacion u ON c.ID_UBICACION = u.ID_UBICACION = 'lugar';
+/* Contar cuantos clientes hay por ciudad */
+SELECT u.Ciudad, count(c.ID_CLIENTES) AS TotalClientes
+FROM clientes c
+JOIN ubicacion u ON c.ID_UBICACION = u.ID_UBICACION = 'lugar'
+GROUP BY u.Ciudad;
+/* Cantidad de ordenes atendidas por cada tecnico */ 
+SELECT t.Nombre, count(o.ID_ORDEN_SERVICIO) AS TotalOrdenes
+FROM tecnicos t 
+JOIN orden_servicio o ON t.ID_TECNICOS = o.ID_TECNICOS 
+GROUP BY t.Nombre 
+ORDER BY TotalOrdenes DESC;
+/* Ver servicios con sus precios */
+SELECT Nombre, Precio FROM servicios;
+/* Productos con precio mayor al promedio */
+SELECT * FROM productos
+WHERE Precio > (SELECT AVG(Precio) FROM productos);
+/* Servicios mas caros */
+SELECT * FROM productos 
+WHERE Precio = (SELECT MAX(Precio) FROM Servicios);
+/* Ver ordenes por cliente */
+SELECT o.ID_ORDEN_SERVICIO, o.Estado, c.Nombre AS cliente 
+FROM orden_servicio o
+JOIN clientes c ON o.ID_CLIENTES = c.ID_CLIENTES
+JOIN tecnicos t ON o.ID_TECNICOS = t.ID_TECNICOS;
+/* Cantidad por ordenes de estado */
+SELECT estado, count(*) AS Total
+FROM orden_servicio 
+GROUP BY estado;
+/* ultimas ordenes registrdas */
+SELECT * FROM orden_servicio
+ORDER BY fecha_inicio DESC 
+LIMIT 5;
+/* Ver los detalles de cada orden */
+SELECT d.ID_DETALLES_ORDEN_SERVICIO, o.ID_ORDEN_SERVICIO, d.Garantia
+FROM detalles_orden_servicio d 
+JOIN orden_servicio o ON d.ID_ORDEN_SERVICIO = o.ID_ORDEN_SERVICIO;
